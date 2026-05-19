6653efd1345e699cb2f45dfc5b797a424bb30ff2
DontCallMeBlack <dcmbch7@gmail.com>
2026-05-19 13:52:50 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -175,7 +175,7 @@ BOSSES = [
     },
   {
         'name': 'Proteus',
-        'respawn_minutes': 1080,
+        'respawn_minutes': 720,
         'window_minutes': 15
     },
     {
@@ -190,7 +190,7 @@ BOSSES = [
     },
     {
         'name': 'Gelebron',
-        'respawn_minutes': 2040,
+        'respawn_minutes': 720,
         'window_minutes': 1680
     },
     {
