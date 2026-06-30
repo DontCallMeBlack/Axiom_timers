00e4de402189b204b332d875e23b764df271fc5f
DontCallMeBlack <dcmbch7@gmail.com>
2026-06-30 14:42:16 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -175,22 +175,22 @@ BOSSES = [
     },
   {
         'name': 'Proteus',
-        'respawn_minutes': 720,
+        'respawn_minutes': 1080,
         'window_minutes': 15
     },
     {
         'name': 'Dino',
-        'respawn_minutes': 720,
+        'respawn_minutes': 1920,
         'window_minutes': 1680
     },
     {
         'name': 'Bloodthorn',
-        'respawn_minutes': 720,
+        'respawn_minutes': 1920,
         'window_minutes': 1680
     },
     {
         'name': 'Gelebron',
-        'respawn_minutes': 720,
+        'respawn_minutes': 1920,
         'window_minutes': 1680
     },
     {
